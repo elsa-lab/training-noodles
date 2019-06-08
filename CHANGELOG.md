61ac1c85dd47ba569ceec11f85b5628141a04419
Shawn Chang <ebola777@yahoo.com.tw>
2019-06-08 17:22:01 +0800

    fix module not found error

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 1.0.1 (2019-06-08)
+
+* Fixed Python module not found error
+
+## 1.0.0 (2019-06-08)
+
+* Added the initial project
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version='1.0.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -133,7 +133,10 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=find_packages(
+        include=['training_noodles.*'],
+        exclude=['contrib', 'docs', 'tests']
+    ),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
